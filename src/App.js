b967585d63e0da572ecc52ccd356f0d5e61f4625
Andrey Sukharev <Andrey.Sukharev@exinity.com>
2023-10-03 16:29:08 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3 from 'web3';
+import React, { useState } from 'react';
+
+import './app.css';
+
+const web3 = new Web3(new Web3.providers.HttpProvider("https://rpc.sepolia.org")); //"http://localhost:8545"));
+
+const dest = '0xB25934228972Fe7a16B2Be33731fC16216a2fff7'; //'0xC1a9D28A1b5D7Ea73b79268b0ee5355B208698A3';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [address, setAddress] = useState("");
+	const [balance, setBalance] = useState("0");
+
+	async function connectHandler() {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			window.open("https://metamask.app.link/dapp/sukharev-andrey.github.io/");
+		}
+	
+		const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		const addr = accounts[0];
+		setAddress(addr);
+
+		const walletBalance = await web3.eth.getBalance(addr);
+		await setBalance(web3.utils.fromWei(walletBalance, "ether"));
+	}
+
+	async function refreshBalance() {
+		const walletBalance = await web3.eth.getBalance(address);
+		await setBalance(web3.utils.fromWei(walletBalance, "ether"));
+	}
+
+	async function makeTransaction() {
+		const { ethereum } = window;
+		const value = web3.utils.toWei(0.01, "ether");
+		console.log("transaction", 'from', address, 'to', dest, 'value', value); 
+
+		try {
+		await ethereum
+			.request(
+				{
+					method: 'eth_sendTransaction',
+					params: [
+						{
+							from: address,
+							to: dest,
+							value: "0x" + Number(value).toString(16),
+						},
+					],
+				}
+			);
+		}
+		catch (err) {}
+	}
+	
+	return (
+		<div className="App">
+			{address !== "" ? 
+				<h1>Connected wallet: {address}</h1> :
+				<h1>Wallet is not connected</h1>
+			}
+			<h1>Balance: {balance} ETH</h1>
+		
+			<button onClick={connectHandler}>Connect</button>
+			<br/>
+			<br/>
+			<button onClick={refreshBalance}>Refresh</button>
+			<br/>
+			<br/>
+			<button onClick={makeTransaction}>Send</button>
+		</div>
+	);
 }
 
 export default App;
